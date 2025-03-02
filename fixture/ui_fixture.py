3a143327e5b1a46ea68f6636b05ec183c77969c4
Kaan Ozdogan <knozdogan@gmail.com>
2025-03-02 22:17:20 +0300

    qa career page added

--- fixture/ui_fixture.py
+++ fixture/ui_fixture.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 from pages.home_page import HomePage
 from pages.career_page import CareerPage
+from pages.qa_career_page import QACareerPage
 
 if not os.getenv('WEB_BASE_URL'):
     load_dotenv()
@@ -15,7 +16,6 @@ BROWSER = os.getenv("BROWSER", "chromium")
 @pytest.fixture(scope="session")
 def setup(playwright):
     """Provides a browser session for UI tests."""
-    playwright.selectors.set_test_id_attribute("id")
     browser = getattr(playwright, BROWSER).launch(headless=False)
     context = browser.new_context(base_url=BASE_URL)
     page = context.new_page()
@@ -25,7 +25,6 @@ def setup(playwright):
 @pytest.fixture(scope="function")
 def home_page(playwright):
     """Provides a browser session for UI tests."""
-    playwright.selectors.set_test_id_attribute("id")
     browser = getattr(playwright, BROWSER).launch(headless=False)
     context = browser.new_context(base_url=BASE_URL)
     new_page = context.new_page()
@@ -39,7 +38,6 @@ def home_page(playwright):
 @pytest.fixture(scope="function")
 def career_page(playwright):
     """Provides a browser session for UI tests."""
-    playwright.selectors.set_test_id_attribute("id")
     browser = getattr(playwright, BROWSER).launch(headless=False)
     context = browser.new_context(base_url=BASE_URL)
     new_page = context.new_page()
@@ -50,5 +48,19 @@ def career_page(playwright):
     page.click_careers_navbar()
     page = CareerPage(new_page)
     page.verify_career_page_is_loaded()
+    yield page
+    browser.close()
+
+@pytest.fixture(scope="function")
+def qa_career_page(playwright):
+    browser = getattr(playwright, BROWSER).launch(headless=False)
+    context = browser.new_context(base_url=BASE_URL)
+    new_page = context.new_page()
+    page = HomePage(new_page)
+    page.navigate()
+    page.accept_all_cookies()
+    page = QACareerPage(new_page)
+    page.navigate()
+    page.verify_qa_career_page_is_loaded()
     yield page
     browser.close()
