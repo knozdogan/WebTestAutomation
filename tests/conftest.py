f05431d9aa44544fdc8dddf3d0340974e7516a10
Kaan Ozdogan <knozdogan@gmail.com>
2025-03-03 23:52:42 +0300

    qa career page fixture added

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,2 +1,2 @@
 from fixture.api_fixture import api_request_context
-from fixture.ui_fixture import setup, career_page, home_page
+from fixture.ui_fixture import setup, career_page, home_page, qa_career_page
